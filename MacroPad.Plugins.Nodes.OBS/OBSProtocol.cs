9b6e9e11513f0768574c3e582882ad1211c56032
Piripe <77517308+Piripe@users.noreply.github.com>
2025-02-18 19:57:47 +0100

    Improved OBSProtocol stability and added automatic (re)connection

--- MacroPad.Plugins.Nodes.OBS/OBSProtocol.cs
+++ MacroPad.Plugins.Nodes.OBS/OBSProtocol.cs
@@ -5,6 +5,7 @@ using OBSWebsocketDotNet;
 using OBSWebsocketDotNet.Types;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,40 +21,81 @@ namespace MacroPad.Plugins.Nodes.OBS
         public event EventHandler<DeviceDetectedEventArgs>? DeviceDetected;
         public event EventHandler<DeviceDetectedEventArgs>? DeviceDisconnected;
 
-        public static OBSWebsocket? Client { get; private set; } = new OBSWebsocket();
+        public static OBSWebsocket? Client { get; private set; } = null;
         public static void Run(Action<OBSWebsocket> x)
         {
-            if (Client != null && Client.IsConnected) x.Invoke(Client);
+            if (Client != null && Client.IsConnected) try { x.Invoke(Client); } catch { }
         }
         public static List<SceneBasicInfo> Scenes = [];
         public static SceneBasicInfo? GetObsScene(string name) => Scenes.FirstOrDefault(x => x.Name == name);
 
+        private CancellationTokenSource? _enabled = null;
+
         public void Enable()
         {
-            if (Client == null || Client.IsConnected) return;
+            if (Client != null && Client.IsConnected) return;
 
-            // Open WS
+            if (_enabled != null) throw new Exception("Can't enable an already enabled protocol.");
+
+            _enabled = new CancellationTokenSource();
+            Client = new OBSWebsocket();
+
             Client.Connected += (s, e) =>
-                {
-                    if (NodeTypes.obsSceneType.Components[0] is ComboBox sceneCB)
+            {
+                if (NodeTypes.obsSceneType.Components[0] is ComboBox sceneCB)
                     {
                         Scenes = Client.GetSceneList().Scenes;
-                        sceneCB.Items = Scenes.Select(x => x.Name).ToArray();
+                    sceneCB.Items.Clear();
+                    foreach (var item in Scenes.Select(x => x.Name))
+                        {
+                            sceneCB.Items.Add(item);
+                        }
                     }
                 };
+            Client.Disconnected += (s, e) =>
+            {
+                if (NodeTypes.obsSceneType.Components[0] is ComboBox sceneCB)
+                {
+                    sceneCB.Items.Clear();
+                }
+
+                if (_enabled != null)
+                {
+                    _enabled = new CancellationTokenSource();
+                    _ = DetectOBSInstance();
+                }
+            };
             Client.SceneListChanged += (s, e) =>
             {
-                Scenes = Client.GetSceneList().Scenes;
+                if (NodeTypes.obsSceneType.Components[0] is ComboBox sceneCB)
+                {
+                    Scenes = Client.GetSceneList().Scenes;
+                    sceneCB.Items.Clear();
+                    foreach (var item in Scenes.Select(x => x.Name))
+                    {
+                        sceneCB.Items.Add(item);
+                    }
+                }
             };
-            Client.ConnectAsync("ws://127.0.0.1:4455", "");
 
+            _ = DetectOBSInstance();
         }
         public void Disable()
         {
-            if (Client == null) return;
+            if (Client == null || _enabled == null) return;
+            _enabled.Cancel();
+            _enabled = null;
             Client.Disconnect();
             Client = null;
         }
 
+        private async Task DetectOBSInstance()
+        {
+            if (!(Client == null || _enabled == null || _enabled.IsCancellationRequested))
+            {
+                await Task.Delay(5000, _enabled.Token).ContinueWith(t => { });
+                if (!(_enabled == null || _enabled.IsCancellationRequested)) Client.ConnectAsync("ws://127.0.0.1:4455", "");
+            }
+        }
     }
 }
